1824ed1236a6a67e891613c0840a232516de80d3
amenabe22 <bdere12345@gmail.com>
2022-09-25 11:44:14 +0300

    new query

--- server/src/resolvers/User.ts
+++ server/src/resolvers/User.ts
@@ -26,6 +26,10 @@ export const signCookie = (user: User) => {
 
 @Resolver(User)
 export class UserResolver {
+  @Query(() => String)
+  async testCiCd() {
+    return "Sup there";
+  }
   // update user push notification token Id
   @Mutation(() => Boolean, { nullable: true })
   @UseMiddleware(isAuthed)
