b7d0d45e5a1a2db61a702e21a9de9f546f59b164
amenabe22 <bdere12345@gmail.com>
2022-09-25 18:40:18 +0300

    added metadata git push

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,17 @@
 import path from "path";
 import vue from "@vitejs/plugin-vue";
-import vitePrerender from "vite-plugin-prerender";
+// import vitePrerender from "vite-plugin-prerender";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vitePrerender({
-      // Required - The path to the vite-outputted app to prerender.
-      staticDir: path.join(__dirname, "dist"),
-      // Required - Routes to render.
-      routes: ["/", "/privacy"],
-    }),
+    // vitePrerender({
+    //   // Required - The path to the vite-outputted app to prerender.
+    //   staticDir: path.join(__dirname, "dist"),
+    //   // Required - Routes to render.
+    //   routes: ["/", "/privacy"],
+    // }),
   ],
   base: "/",
   define: {
