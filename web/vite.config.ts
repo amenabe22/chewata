40dca5b01201c751a69e493588a04f673ba8e674
amenabe22 <bdere12345@gmail.com>
2022-09-25 19:13:52 +0300

    update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,9 @@
-import path from "path";
-import vue from "@vitejs/plugin-vue";
-// import vitePrerender from "vite-plugin-prerender";
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // vitePrerender({
-    //   // Required - The path to the vite-outputted app to prerender.
-    //   staticDir: path.join(__dirname, "dist"),
-    //   // Required - Routes to render.
-    //   routes: ["/", "/privacy"],
-    // }),
-  ],
+  plugins: [vue()],
   base: "/",
   define: {
     "process.env": process.env,
