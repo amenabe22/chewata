218e47383c83d4ab21e601f1e22add23c8e94b81
amenabe22 <bdere12345@gmail.com>
2022-10-03 01:15:27 +0300

    change to privacy

--- web/src/views/Privacy.vue
+++ web/src/views/Privacy.vue
@@ -20,10 +20,6 @@
         for visitors to our website with regards to the information that they
         shared and/or collect in chewata. This policy is not applicable to any
         information collected offline or via channels other than this website.
-        Our Privacy Policy was created with the help of the
-        <a href="https://www.privacypolicygenerator.info/"
-          >Free Privacy Policy Generator</a
-        >.
       </p>
 
       <h2>Consent</h2>
